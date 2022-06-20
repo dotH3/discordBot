106ad12ca6803c313ed1055cf1d1732df75684e1
h3 <joaquinbastias321@gmail.com>
2022-06-20 00:03:42 -0300

    Commands Handler

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const { Client, Intents, MessageEmbed } = require("discord.js");
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+const { Client, Intents, MessageEmbed, Collection} = require("discord.js");
+const client = new Client({intents: ['GUILDS', 'GUILD_MESSAGES']});
+const prefix = '!';
+
+client.commands = new Collection();
+const commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(file of commands){
+  const commandName = file.split('.')[0];
+  const command = require(`./commands/${commandName}`);
+  client.commands.set(commandName, command)
+}
+
 
 require("dotenv").config();
 const token = process.env.TOKEN;
-const admin_id = process.env.ADMIN_ID;
-const prefix = process.env.PREFIX;
-const color = "0x66b3ff";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -15,43 +20,32 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-  const messageArray = message.content.split(" ");
-  const command = messageArray[0].slice(1);
-  const mention = message.mentions.users.first() || message.author;
-  const avatar = mention.displayAvatarURL({ size: 1024, dynamic: true });
-  const server = message.guild;
-  const serveroles = message.guild.roles.cache
-    .sort((a, b) => b.position - a.position)
-    .map((role) => role.toString());
+  // const server = message.guild;
+  // const serveroles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map((role) => role.toString());
 
-  if (command == "ping") {
-    message.channel.send({ content: "pong! 🏓 " + mention });
-  }
+  if(message.content.startsWith(prefix)){
+    const args = message.content.slice(prefix.length).trim().split(/ + /g);
+    const commandName = args.shift();
+    const command = client.commands.get(commandName);
+    if(!command)return
+    command.run(client, message, args);
+  };
 
-  if (command == "avatar") {
-    const embed = new MessageEmbed()
-      .setTitle("Avatar")
-      //.setAuthor(message.author.username)
-      .setImage(avatar)
-      .setTimestamp()
-      .setColor(color);
-    message.channel.send({ embeds: [embed] });
-  }
-  if (command == "server") {
-    const embed = new MessageEmbed()
-      .setThumbnail(server.iconURL)
-      .setAuthor({
-        name: server.name,
-        iconURL: server.iconURL(),
-      })
-      .addField("ID", server.id)
-      .addField("Creado el", server.createdAt.toLocaleDateString())
-      .addField("Miembros", server.memberCount.toString())
-      .addField("Roles", serveroles.toString())
-      .setColor(color)
-      .setTimestamp();
-    message.channel.send({ embeds: [embed] });
-  }
+  // if (command == "server") {
+  //   const embed = new MessageEmbed()
+  //     .setThumbnail(server.iconURL)
+  //     .setAuthor({
+  //       name: server.name,
+  //       iconURL: server.iconURL(),
+  //     })
+  //     .addField("ID", server.id)
+  //     .addField("Creado el", server.createdAt.toLocaleDateString())
+  //     .addField("Miembros", server.memberCount.toString())
+  //     .addField("Roles", serveroles.toString())
+  //     .setColor(color)
+  //     .setTimestamp();
+  //   message.channel.send({ embeds: [embed] });
+  // }
 });
 
 client.login(token);
